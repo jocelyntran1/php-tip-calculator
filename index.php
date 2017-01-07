3c9c102e6f4df89dd5175595c90f690df14c42d4
Jocelyn Tran <jocelyn.tran1@gmail.com>
2017-01-06 21:26:26 -0800

    Implement required functionalities

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <title> Jocelyn Tran's Tip Calculator </title>
+  <body>
+    <h1> Tip Calculator </h1>
+
+
+    <form action="index.php" method="post">
+    <p>Bill subtotal: 
+
+      <?php
+        if( isset( $_POST["subtotal"] )) {
+          $bill = $_POST['subtotal'];
+          echo "<input type=\"number\" name=\"subtotal\"value=\"$bill\" required>";
+        } else {
+          echo "<input type=\"number\" name=\"subtotal\" required>";
+        }
+      ?>
+
+    </p>
+
+    <p> Tip Percentage:
+
+    <?php
+
+      $tips = array( 10, 15, 20 );
+      foreach( $tips as $i ) {
+        if( isset( $_POST["tip_percent"] ) && $_POST["tip_percent"] == $i ) {
+          echo "<input type=\"radio\" name=\"tip_percent\" required checked value=\"$i\"/> $i%";
+        } else {
+          echo "<input type=\"radio\" name=\"tip_percent\" required value=\"$i\"/> $i%";
+        }
+      } 
+
+    ?>
+    </p>
+    <input type=submit>
+    </form>
+
+    <br>
+
+    <?php
+    if( isset( $_POST["subtotal"]) && isset( $_POST["tip_percent"])) {
+      if( $_POST["subtotal"] > 0 ) {
+
+        $tip = $_POST["tip_percent"]/100 * $_POST["subtotal"];
+        echo "Tip: $" . $tip;
+        echo "Total: $". ($tip + $_POST["subtotal"]);
+      } else {
+        echo "Please enter a bill amount above $0";
+      }
+
+    }
+    ?>  
+  </form>
+
+</body>
+</html>
